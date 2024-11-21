3d5d1e6191309891890b07ff60ee76a5d4ecac17
Zyad Elbehiry <zyadelbehiry@gmail.com>
2024-11-21 17:36:37 +0200

    edit backend

--- Back-End-main/Task Management App Frontend/src/Components/UI/BrandIcon/BrandIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import style from "./BrandIcon.module.css"
-const BrandIcon = () => {
-  return (
-    <span className={style["brand-icon"]}>OoO</span>
-  )
-}
-
-export default BrandIcon
